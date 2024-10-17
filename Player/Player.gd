549be266a0a1c7cfcc3d64878a8555ff688e2d7b
mariacook543 <mariacook543@gmail.com>
2024-10-17 14:20:01 -0500

    acceleration

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,12 +1,18 @@
 extends CharacterBody2D
 
+const accel = 400
+const max_spd = 150
+const friction = 400
+
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector
+		velocity += input_vector * accel * delta	
 	else:
-		velocity = Vector2.ZERO
-	move_and_collide(velocity)
+		velocity = velocity.move_toward(Vector2.ZERO, friction)
+	move_and_collide(velocity * delta)
