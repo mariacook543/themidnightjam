d64f705618ed2a78dd433c2100806081cc9dce93
mariacook543 <mariacook543@gmail.com>
2024-10-17 09:01:42 -0500

    more player movement

---
+++ Player/Player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+vvar velocity = Vector2.ZERO
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = 4
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -4
+	else:
+		velocity.x = 0
+	
+	move_and_collide(velocity)
