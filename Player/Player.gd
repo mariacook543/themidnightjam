e654422157196c83e0c9fb207ea3d1f19b06bf08
mariacook543 <mariacook543@gmail.com>
2024-10-19 12:28:06 -0500

    animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,8 +5,12 @@ const max_spd = 150
 const friction = 400
 
 var animationplayer = null
+var animationtree = null
+var animationstate = null
 func _ready():
 	animationplayer = $AnimationPlayer
+	animationtree = $AnimationTree
+	animationstate = animationtree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -15,13 +19,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		if input_vector.x > 0:
-			animationplayer.play("pigwalkright")
-		else:
-			animationplayer.play("pigwalkleft")
+		animationtree.set("parameters/Idle/blend_position", input_vector)
+		animationtree.set("parameters/Walk/blend_position", input_vector)
+		animationstate.travel("Walk")
 		velocity = velocity.move_toward(input_vector * max_spd, accel * delta)
 		velocity += input_vector * accel * delta	
 	else:
-		animationplayer.play("pigidleright")
+		animationstate.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, friction)
 	move_and_slide()
